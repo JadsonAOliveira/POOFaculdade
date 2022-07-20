d125e6cfa007a98219f9cddd34100c77ed54a73d
JadsonAOliveira <91754639+JadsonAOliveira@users.noreply.github.com>
2022-07-20 20:35:03 -0300

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -3,6 +3,11 @@ package br.edu.ifs.academico;
 public class Professor extends Pessoa {
     private String formacao;
 
+    @Override
+    public String toString() {
+        return super.toString() + "Professor{" + "formacao=" + formacao + '}';
+    }
+
     public String getFormacao() {
         return formacao;
     }
