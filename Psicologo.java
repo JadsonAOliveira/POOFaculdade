66d2d4bd769c94155679dbbfbfc1bb063c1baace
JadsonAOliveira <91754639+JadsonAOliveira@users.noreply.github.com>
2022-07-20 20:36:16 -0300

    Update Psicologo.java

--- Psicologo.java
+++ Psicologo.java
@@ -4,19 +4,25 @@ public class Psicologo extends Pessoa{
     private int carga_horaria;
     private int numero_conselho;
 
+    @Override
+    public String toString() {
+        return super.toString() + "Psicologo{" + "carga_horaria=" + carga_horaria + ", numero_conselho=" + numero_conselho + '}';
+    }
+
     public int getCarga_horaria() {
+
         return carga_horaria;
     }
 
     public void setCarga_horaria(int carga_horaria) {
+
         this.carga_horaria = carga_horaria;
     }
 
-    public int getNumero_conselho() {
+    public int getNumero_conselho(){
         return numero_conselho;
     }
-
-    public void setNumero_conselho(int numero_conselho) {
+    public void setNumero_conselho(int numero_conselho){
         this.numero_conselho = numero_conselho;
     }
 }
