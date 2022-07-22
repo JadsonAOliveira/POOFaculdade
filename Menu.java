ee6eebf1e82090a974f6fa54ae4b43637d344d65
JadsonAOliveira <91754639+JadsonAOliveira@users.noreply.github.com>
2022-07-21 22:16:01 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -9,11 +9,15 @@ public class Menu {
                 + "02 - Cadastrar Professor \n"
                 + "03 - Cadastrar Psicólogo \n"
                 + "04 - Cadastrar Técnico \n"
+                + "05 - Cadastrar Inspetor \n"
+                + "06 - Cadastrar Avaliador \n"
                 + "\n"
                 + "11 - Listar Alunos \n"
                 + "12 - Listar Professores \n"
                 + "13 - Listar Psicólogos \n"
                 + "14 - Listar Técnicos \n"
+                + "15 - Listar Inspetores \n"
+                + "16 - Listar Avaliadores \n"
                 + "0 - Sair";
     }
 }
