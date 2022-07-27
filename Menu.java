d7e8f66c6bf651ac1a04c12fb9285e52a8cd9536
JadsonAOliveira <91754639+JadsonAOliveira@users.noreply.github.com>
2022-07-27 20:08:06 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,23 +1,20 @@
 package br.edu.ifs.academico;
 
-public class Menu {
-    
-    public String getMenu(){
+public abstract class Menu {
+
+    public static String getMenu(){
         return "****MENU**** \n"
                 + "SELECIONE UMA OPÇÃO \n"
                 + "01 - Cadastrar Aluno \n"
                 + "02 - Cadastrar Professor \n"
                 + "03 - Cadastrar Psicólogo \n"
                 + "04 - Cadastrar Técnico \n"
-                + "05 - Cadastrar Inspetor \n"
-                + "06 - Cadastrar Avaliador \n"
                 + "\n"
                 + "11 - Listar Alunos \n"
                 + "12 - Listar Professores \n"
                 + "13 - Listar Psicólogos \n"
-                + "14 - Listar Técnicos \n"
-                + "15 - Listar Inspetores \n"
-                + "16 - Listar Avaliadores \n"
+                + "14 - Listar Técnicos\n"
                 + "0 - Sair";
+
     }
 }
